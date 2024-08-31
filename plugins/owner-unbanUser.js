58bc0f2d6e8852ae072c026338c16233eb1a9040
wavenube <wavenub3@gmail.com>
2024-08-31 12:15:30 +0100

    Update owner-unbanUser.js

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -5,14 +5,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `✳️ Tag or mention the user to unban`
+    if (!who) throw `✳️ Hazle tag o mencion al usuario a desbanear`
     let users = global.db.data.users
     users[who].banned = false
     conn.reply(m.chat, `
 ✅ UNBAN
 
 ───────────
-@${who.split`@`[0]} has been unbanned`, m, { mentions: [who] })
+@${who.split`@`[0]} ha sido desbaneado`, m, { mentions: [who] })
 }
 handler.help = ['unban @user']
 handler.tags = ['owner']
