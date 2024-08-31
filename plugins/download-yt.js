2ead237e0756a2a03fadf6e0598ecd0919f21711
wavenube <wavenub3@gmail.com>
2024-09-01 00:32:31 +0100

    Create download-yt.js

---
+++ plugins/download-yt.js
@@ -0,0 +1,52 @@
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+
+const handler = async function(m, { conn, args, usedPrefix }) {
+    if (!args[0]) throw 'Por favor, proporciona un enlace de YouTube.';
+
+    const youtubeLink = args[0];
+    const mensaje = `¿Cómo deseas descargar el contenido de YouTube?`;
+
+    await sendInteractiveMessage(m, conn, mensaje, youtubeLink, usedPrefix);
+};
+
+// Función para enviar el mensaje interactivo con botones
+async function sendInteractiveMessage(m, conn, mensaje, youtubeLink, usedPrefix) {
+    // Generar el mensaje interactivo con botones
+    const msg = generateWAMessageFromContent(m.chat, {
+        viewOnceMessage: {
+            message: {
+                interactiveMessage: {
+                    body: { text: mensaje },
+                    footer: { text: 'Selecciona una opción' }, // Pie de página opcional
+                    nativeFlowMessage: {
+                        buttons: [
+                            {
+                                name: 'quick_reply',
+                                buttonParamsJson: JSON.stringify({
+                                    display_text: 'DESCARGAR VIDEO',
+                                    id: `${usedPrefix}getvid ${youtubeLink}`
+                                })
+                            },
+                            {
+                                name: 'quick_reply',
+                                buttonParamsJson: JSON.stringify({
+                                    display_text: 'DESCARGAR AUDIO',
+                                    id: `${usedPrefix}getaud ${youtubeLink}`
+                                })
+                            },
+                        ],
+                        messageParamsJson: "",
+                    },
+                },
+            },
+        }
+    }, { userJid: conn.user.jid, quoted: m });
+
+    // Enviar el mensaje
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+}
+
+// Configuración del comando
+handler.command = /^(yt)$/i;
+
+export default handler;
