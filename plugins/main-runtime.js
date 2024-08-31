46d94fdd8e09f8fc3f0d4f94eac26cfc6b697c68
wavenube <wavenub3@gmail.com>
2024-08-31 12:19:58 +0100

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`🎭 *Bot active duration*  \n\n${muptime}`) 
+   m.reply(`🎭 *Bot activo durante*  \n\n${muptime}`) 
 }
 handler.help = ['runtime']
 handler.tags = ['main']
