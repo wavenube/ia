9404781cb7a28ebad6805ea84a8b3fd61bdf8b39
wavenube <wavenub3@gmail.com>
2024-09-01 00:43:46 +0100

    Update download-yt.js

--- plugins/download-yt.js
+++ plugins/download-yt.js
@@ -12,44 +12,44 @@ const handler = async function (m, { conn, args, usedPrefix }) {
 
 // Función para enviar el mensaje interactivo con botones
 async function sendInteractiveMessage(m, conn, mensaje, youtubeLink, usedPrefix) {
-    const buttons = [
-        {
-            buttonId: `${usedPrefix}audio ${youtubeLink}`, // Ejecuta el comando .audio <link>
-            buttonText: { displayText: 'DESCARGAR AUDIO' },
-            type: 1
-        },
-        {
-            buttonId: `${usedPrefix}video ${youtubeLink}`, // Ejecuta el comando .video <link>
-            buttonText: { displayText: 'DESCARGAR VIDEO' },
-            type: 1
-        }
-    ];
-
     const buttonMessage = {
         text: mensaje,
         footer: 'Selecciona una opción',
-        buttons: buttons,
+        buttons: [
+            {
+                buttonId: `${usedPrefix}audio ${youtubeLink}`, // Ejecuta el comando .audio <link>
+                buttonText: { displayText: 'DESCARGAR AUDIO' },
+                type: 1
+            },
+            {
+                buttonId: `${usedPrefix}video ${youtubeLink}`, // Ejecuta el comando .video <link>
+                buttonText: { displayText: 'DESCARGAR VIDEO' },
+                type: 1
+            }
+        ],
         headerType: 1
+    };
+
+    const messageContent = {
+        interactiveMessage: {
+            body: buttonMessage.text,
+            footer: buttonMessage.footer,
+            nativeFlowMessage: {
+                buttons: buttonMessage.buttons.map(btn => ({
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                        display_text: btn.buttonText.displayText,
+                        id: btn.buttonId
+                    })
+                })),
+                messageParamsJson: "",
+            },
+        }
     };
 
     const msg = generateWAMessageFromContent(m.chat, {
         viewOnceMessage: {
-            message: {
-                interactiveMessage: {
-                    body: buttonMessage.text,
-                    footer: buttonMessage.footer,
-                    nativeFlowMessage: {
-                        buttons: buttons.map(btn => ({
-                            name: 'quick_reply',
-                            buttonParamsJson: JSON.stringify({
-                                display_text: btn.buttonText.displayText,
-                                id: btn.buttonId
-                            })
-                        })),
-                        messageParamsJson: "",
-                    }
-                }
-            }
+            message: messageContent
         }
     }, { userJid: conn.user.jid, quoted: m });
 
