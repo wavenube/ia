397216ecc789ccf43404fe1e98ded504f1311227
wavenube <wavenub3@gmail.com>
2024-08-31 02:10:58 +0100

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,12 +2,12 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `🛸 *Enter serial number*\nCheck your serial number with the command...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `🛸 *Ingresa tu numero de serie*\nMira tu numero de serie con el comando...\n\n*${usedPrefix}nserie*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw '⚠️ *Incorrect serial number*'
+  if (args[0] !== sn) throw '⚠️ *Su numero de serie es incorrecto*'
   user.registered = false
-  m.reply(`✅ Register eliminated`)
+  m.reply(`✅ Se ha eliminado tu registro`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rg']
