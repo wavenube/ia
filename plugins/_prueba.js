00ec2da922a044eb9c7757ba528445c2665c067f
wavenube <wavenub3@gmail.com>
2024-08-31 01:55:57 +0100

    Update _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -19,27 +19,16 @@ const handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     const totalreg = Object.keys(global.db.data.users).length;
     const rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
 
-    const pp = global.imagen1 || './media/abyss.png';  // Imagen predeterminada
+    const pp = global.imagen1 || './media/contact.png';  // Imagen predeterminada
     const more = String.fromCharCode(8206);
     const readMore = more.repeat(850);
 
     const menuText = `
       *Bienvenido al Menú de Comandos*
       
-      *Usuario:* ${name}
-      *Nivel:* ${level}
-      *Experiencia:* ${exp} / ${xp}
-      *Rol:* ${role}
-      *Limite:* ${limit}
-      *Registrados:* ${totalreg} / ${rtotalreg}
-      
-      *Fecha:* ${week}, ${date}
-      *Uptime:* ${uptime}
-      
-      ${readMore}
       
       *Comandos Disponibles:*
-      ☆  %cmd 
+      ☆ 
       
       *Made by ♡ Shizo*
     `.trim();
