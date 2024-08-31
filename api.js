e1b8432cd0f280c35c304f85aef63a35238e3c58
wavenube <wavenub3@gmail.com>
2024-08-31 12:54:38 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,36 +1,34 @@
-import {watchFile, unwatchFile} from 'fs';
+import { watchFile, unwatchFile } from 'fs';
 import chalk from 'chalk';
-import {fileURLToPath} from 'url';
-import fs from 'fs';
-import cheerio from 'cheerio';
+import { fileURLToPath } from 'url';
 import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-/* Para agregar más APIs asegurate de poner global.apiname = ['APIKey'] */ /* By Skid 🤑 */
+// Configuración de APIs
+global.openai_key = 'sk-0';  // API Key de OpenAI
+global.openai_org_id = 'org-3';  // ID de organización de OpenAI
 
-global.openai_key = 'sk-0';
-/* Obtén tu API Key en este enlace: https://platform.openai.com/account/api-keys */
-
-global.openai_org_id = 'org-3';
-/* Obtén tu ID de organización en este enlace: https://platform.openai.com/account/org-settings */
-
-global.MyApiRestBaseUrl = 'https://api.cafirexos.com'; // Bloqueo de IP -> usar esta para no ser bloqueado: 'https://api-brunosobrino.onrender.com';
+global.MyApiRestBaseUrl = 'https://api.cafirexos.com';
 global.MyApiRestApikey = 'BrunoSobrino';
 
 global.MyApiRestBaseUrl2 = 'https://api-brunosobrino-dcaf9040.koyeb.app';
-
 global.MyApiRestBaseUrl3 = 'https://api-brunosobrino.onrender.com'; 
 
+// API Keys aleatorias para Zens, XTeam, Neoxr, etc.
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
+global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())];
+
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
+global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())];
+
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
-global.lolkeysapi = ['GataDiosV2']; // ['BrunoSobrino_2']
+global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())];
+
+global.lolkeysapi = ['GataDiosV2'];  // ['BrunoSobrino_2']
 global.itsrose = ['4b146102c4d500809da9d1ff'];
 
+// APIs y sus Keys
 global.APIs = {
   CFROSAPI: 'https://api.cafirexos.com',
   xteam: 'https://api.xteam.xyz',
@@ -51,12 +49,13 @@ global.APIs = {
   xyroinee: 'https://api.xyroinee.xyz',
   nekobot: 'https://nekobot.xyz',
   BK9: 'https://apii.bk9.site'
-},
+};
+
 global.APIKeys = {
-  'https://api.xteam.xyz': `${keysxteam}`,
+  'https://api.xteam.xyz': `${global.keysxteam}`,
   'https://api.lolhuman.xyz': 'GataDios',
-  'https://api.neoxr.my.id': `${keysneoxr}`,
-  'https://api.zahwazein.xyz': `${keysxxx}`,
+  'https://api.neoxr.my.id': `${global.keysneoxr}`,
+  'https://api.zahwazein.xyz': `${global.keysxxx}`,
   'https://api-fgmods.ddns.net': 'fg-dylux',
   'https://api.botcahx.biz.id': 'Admin',
   'https://api.ibeng.tech/docs': 'tamvan',
@@ -65,14 +64,11 @@ global.APIKeys = {
   'https://api.xyroinee.xyz': 'uwgflzFEh6',
   'https://apikasu.onrender.com': 'ApiKey'
 };
-    const results = Object.keys(emotttt).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
-    if (!results.length) return '';
-    else return emotttt[results[0][0]];
-  }};
 
+// Actualización en caliente del archivo
 const file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
   unwatchFile(file);
-  console.log(chalk.redBright('Update \'api.js\''));
+  console.log(chalk.redBright('Update "api.js"'));
   import(`${file}?update=${Date.now()}`);
 });
