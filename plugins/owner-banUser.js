0b06165aac0eeed81a1441ddecc4be68d222fbf6
wavenube <wavenub3@gmail.com>
2024-08-31 12:16:56 +0100

    Update owner-banUser.js

--- plugins/owner-banUser.js
+++ plugins/owner-banUser.js
@@ -5,14 +5,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `🎭 Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
+    if (!who) throw `🎭 Hazle tag o mencion a el usuario que deseas banear\n\n📌 Ejemplo : ${usedPrefix + command} @user`
     let users = global.db.data.users
     users[who].banned = true
     conn.reply(m.chat, `
 ✅ BANNED
 
 ───────────
-@${who.split`@`[0]} you will no longer be able to use my commands `, m, { mentions: [who] })
+@${who.split`@`[0]} lo siento rey, ya no podras abusar de mi `, m, { mentions: [who] })
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
