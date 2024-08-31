37a3fa93453229e0153a7e475490407a8963bc02
wavenube <wavenub3@gmail.com>
2024-08-31 12:18:34 +0100

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'So youre a bot as an admin'
-  if (isAdmin) throw 'Already'
+  if (m.fromMe) throw 'Gracias por el admin.'
+  if (isAdmin) throw 'Ahora'
 await m.reply(`Awwww 🥰`)
   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
 }
