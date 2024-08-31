8366a5cdb2f6e34002aefafeeb25ef3563f2a769
wavenube <wavenub3@gmail.com>
2024-08-31 12:16:00 +0100

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ The bot was deactivated in this group')
+    m.reply('✅ El Bot ha sido desactivado en este chat')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
