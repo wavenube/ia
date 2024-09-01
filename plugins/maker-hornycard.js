2b73c2344798d6e59657866bc58b919c12a299c9
wavenube <wavenub3@gmail.com>
2024-09-01 01:44:02 +0100

    Create maker-hornycard.js

---
+++ plugins/maker-hornycard.js
@@ -0,0 +1,23 @@
+const handler = async (m, { conn }) => {
+  // Determina el ID del usuario mencionado, el ID del mensaje citado, o el ID del remitente
+  const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+
+  // Envía una imagen generada con la API de canvas
+  await conn.sendFile(
+    m.chat,
+    global.API('https://some-random-api.com', '/canvas/horny', {
+      avatar: await conn.profilePictureUrl(who, 'image').catch(() => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    }),
+    'hornycard.png',
+    'Aquí está tu imagen'
+    // Se eliminó la traducción, se usa texto estático en lugar de texto traducido
+    , 
+    m
+  );
+};
+
+handler.help = ['hornycard', 'hornylicense'];
+handler.tags = ['maker'];
+handler.command = /^(horny(card|license))$/i;
+
+export default handler;
