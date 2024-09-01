7351d418c983999676d654f1cd73ec24d49dd8fb
wavenube <wavenub3@gmail.com>
2024-09-01 01:50:39 +0100

    Update maker-pixelar.js

--- plugins/maker-pixelar.js
+++ plugins/maker-pixelar.js
@@ -2,14 +2,23 @@ const handler = async (m, { conn, text }) => {
   // Verifica si el texto está presente
   if (!text) throw 'No Text';
 
-  // Define la URL del avatar de manera predeterminada en caso de error
-  const avatarUrl = await conn.profilePictureUrl(m.sender, 'image').catch(() => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+  // Define el avatar de la foto respondida, si existe
+  const quotedMessage = m.quoted ? m.quoted : null;
+  let imageUrl;
+
+  // Si hay un mensaje citado y contiene una imagen, usa esa imagen
+  if (quotedMessage && quotedMessage.message && quotedMessage.message.imageMessage) {
+    imageUrl = quotedMessage.message.imageMessage.url;
+  } else {
+    // Si no hay imagen, usa el perfil del usuario como fallback
+    imageUrl = await conn.profilePictureUrl(m.sender, 'image').catch(() => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+  }
 
   // Envía una imagen generada con la API de canvas
   await conn.sendFile(
     m.chat,
     global.API('https://some-random-api.com', '/canvas/pixelate', {
-      avatar: avatarUrl,
+      avatar: imageUrl,
       comment: text,
       username: conn.getName(m.sender),
     }),
