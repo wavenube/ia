1722cb9efecae89556a1183033f98d5642695edb
wavenube <wavenub3@gmail.com>
2024-09-01 01:29:28 +0100

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -38,13 +38,13 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                                         header: video.title,
                                         title: video.author.name,
                                         description: 'Descargar MP3',
-                                        id: `${prefijo}play.1 ${video.url}`
+                                        id: `${prefijo}audio ${video.url}`
                                     },
                                     {
                                         header: video.title,
                                         title: video.author.name,
                                         description: 'Descargar MP4',
-                                        id: `${prefijo}play.2 ${video.url}`
+                                        id: `${prefijo}video ${video.url}`
                                     }
                                 ]
                             }))
