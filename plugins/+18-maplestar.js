b7e576234b19128f1511b4d2b8baf0159ae1902d
wavenube <wavenub3@gmail.com>
2024-09-04 01:51:17 +0100

    Update +18-maplestar.js

--- plugins/+18-maplestar.js
+++ plugins/+18-maplestar.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
-const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import("@whiskeysockets/baileys")).default;
+import { generateWAMessageFromContent, proto } from "@whiskeysockets/baileys";
 
-// Define tus enlaces de video aquí
 const VIDEO_URLS = [
     'https://qu.ax/scZw.mp4',
     'https://qu.ax/sKfw.mp4',
     'https://qu.ax/urcH.mp4',
+    'https://qu.ax/PZCF.mp4',
+    'https://qu.ax/rvnd.mp4',
+    'https://qu.ax/OxHZ.mp4',
+    'https://qu.ax/aUXT.mp4',
+    'https://qu.ax/wbJN.mp4',
+    'https://qu.ax/fJf.mp4',
+    'https://qu.ax/bFdc.mp4',
+    'https://qu.ax/vHNs.mp4',
+    'https://qu.ax/uppk.mp4',
     'https://qu.ax/YnM.mp4',
 ];
 
@@ -13,16 +20,15 @@ let handler = async (message, { conn }) => {
     try {
         shuffleArray(VIDEO_URLS);
         let selectedVideos = VIDEO_URLS.slice(0, 7);
-        let videoMessages = await Promise.all(selectedVideos.map(url => createVideoMessage(url, conn)));
-        let results = videoMessages.map((videoMessage, index) => ({
+        let results = selectedVideos.map((url, index) => ({
             body: proto.Message.InteractiveMessage.Body.fromObject({ text: '' }),
             footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: `*❧ By ${global.wm}*` }),
             header: proto.Message.InteractiveMessage.Header.fromObject({
                 title: `Video ${index + 1}`,
-                hasMediaAttachment: true,
-                videoMessage: videoMessage
+                hasMediaAttachment: false
             }),
-            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] }),
+            urlMessage: { url }
         }));
         const responseMessage = generateWAMessageFromContent(message.chat, {
             viewOnceMessage: {
@@ -45,21 +51,11 @@ let handler = async (message, { conn }) => {
         await conn.sendMessage(message.chat, { text: error.toString() }, { quoted: message });
     }
 };
+
 handler.help = ['videos'];
 handler.tags = ['general'];
 handler.command = /^(videos)$/i;
 export default handler;
-
-async function createVideoMessage(url, conn) {
-    try {
-        const response = await axios.get(url, { responseType: 'arraybuffer' });
-        const buffer = response.data;
-        const { videoMessage } = await generateWAMessageContent({ video: buffer }, { upload: conn.waUploadToServer });
-        return videoMessage;
-    } catch (error) {
-        throw new Error(`Error al crear el mensaje de video: ${error.message}`);
-    }
-}
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
