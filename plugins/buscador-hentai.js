4004808c1aa25c2451a70de64c09235fdc2c0594
wavenube <wavenub3@gmail.com>
2024-09-04 17:26:58 +0100

    Create buscador-hentai.js

---
+++ plugins/buscador-hentai.js
@@ -0,0 +1,52 @@
+import cheerio from 'cheerio';
+import axios from 'axios';
+
+const handler = async (m, {conn, text}) => {
+  if (!global.db.data.chats[m.chat].modohorny && m.isGroup) throw 'Este comando no está habilitado en este chat.';
+  if (!text) throw 'Por favor, ingresa un término para buscar.';
+  
+  const searchResults = await searchHentai(text);
+  let teks = searchResults.result.map((v, i) => `
+${i+1}. *_${v.title}_*
+↳ 📺 *_Vistas:_* ${v.views}
+↳ 🎞️ *_Link:_* ${v.url}`).join('\n\n');
+  
+  let randomThumbnail;
+  if (searchResults.result.length > 0) {
+    const randomIndex = Math.floor(Math.random() * searchResults.result.length);
+    randomThumbnail = searchResults.result[randomIndex].thumbnail;
+  } else {
+    randomThumbnail = 'https://pictures.hentai-foundry.com/e/Error-Dot/577798/Error-Dot-577798-Zero_Two.png';
+    teks = 'No se encontraron resultados.';
+  }
+
+  conn.sendFile(m.chat, randomThumbnail, 'error.jpg', teks, m);
+};
+
+handler.command = /^(hentaisearch|searchhentai)$/i;
+export default handler;
+
+async function searchHentai(search) {
+  return new Promise((resolve, reject) => {
+    axios.get('https://hentai.tv/?s=' + search).then(async ({data}) => {
+      const $ = cheerio.load(data);
+      const result = {};
+      const res = [];
+      result.coder = 'rem-comp';
+      result.result = res;
+      result.warning = 'It is strictly forbidden to reupload this code, copyright © 2022 by rem-comp';
+      $('div.flex > div.crsl-slde').each(function(a, b) {
+        const _thumbnail = $(b).find('img').attr('src');
+        const _title = $(b).find('a').text().trim();
+        const _views = $(b).find('p').text().trim();
+        const _url = $(b).find('a').attr('href');
+        const hasil = {thumbnail: _thumbnail, title: _title, views: _views, url: _url};
+        res.push(hasil);
+      });
+      resolve(result);
+    }).catch((err) => {
+      console.log(err);
+      reject(err);
+    });
+  });
+}
