1d6b7f70b4c77dcea0334000b2e52d71612be3ea
wavenube <wavenub3@gmail.com>
2024-09-05 15:41:35 +0100

    Update bebot-broadcastBots.js

--- plugins/bebot-broadcastBots.js
+++ plugins/bebot-broadcastBots.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, usedPrefix, text }) => {
     let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
     let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
     let teks = text ? text : cc.text
-    conn.reply(m.chat, `✅ ${mssg.txdone} *${mssg.total}:* *${users.length}* sub bots\n\n${users.map((v, i) => `*${i + 1}.* wa.me/${v.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')}`.trim(), m)
+    conn.reply(m.chat, `✅ ${m.txdone} *${m.total}:* *${users.length}* sub bots\n\n${users.map((v, i) => `*${i + 1}.* wa.me/${v.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')}`.trim(), m)
     
-    let content = conn.cMod(m.chat, cc, /txbot|broadcast/i.test(teks) ? teks : `${mssg.tx.toUpperCase()} ┃ SUB BOTS\n_____________________\n\n${teks}`)
+    let content = conn.cMod(m.chat, cc, /txbot|broadcast/i.test(teks) ? teks : `${m.tx.toUpperCase()} ┃ SUB BOTS\n_____________________\n\n${teks}`)
     for (let id of users) {
       await delay(1500)
       await conn.copyNForward(id, content, true)
