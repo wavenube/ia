48fa20e91c5f637b88269b7f0abb47a0e75e620c
wavenube <wavenub3@gmail.com>
2024-09-05 15:40:22 +0100

    Update bebot-botlist.js

--- plugins/bebot-botlist.js
+++ plugins/bebot-botlist.js
@@ -4,9 +4,9 @@ async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map(conn => conn.user)])]
   let b = users.map((v, i) => `_*${i + 1}.*_ *${v.name}*\nwa.me/${v.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')
  m.reply(` 
-≡ *${mssg.botlist}*
+≡ *${m.botlist}*
 
-▢ *${mssg.total}:* ${users.length} 
+▢ *${m.total}:* ${users.length} 
 
 ${b}`) 
   
