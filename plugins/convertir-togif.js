15b7c42e4a7122748f08adb92926ef9a7e5ee7cf
wavenube <wavenub3@gmail.com>
2024-09-05 18:47:32 +0100

    Create convertir-togif.js

---
+++ plugins/convertir-togif.js
@@ -0,0 +1,17 @@
+const handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.quoted) throw '*Debes responder a un video para convertirlo en GIF con audio.*';
+  
+  const q = m.quoted || m;
+  const mime = (q.msg || q).mimetype || '';
+  
+  if (!/(mp4)/.test(mime)) throw `*El archivo debe ser un video MP4, no se puede convertir el archivo con tipo ${mime}.*`;
+  
+  m.reply('Por favor espera mientras convierto el video...');
+  
+  const media = await q.download();
+  
+  conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: 'Aquí tienes tu GIF con audio.' }, { quoted: m });
+};
+
+handler.command = ['togifaud'];
+export default handler;
