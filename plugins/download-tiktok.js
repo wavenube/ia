5b686fc540a2a795e7740e02c362c0db6217964a
wavenube <wavenub3@gmail.com>
2024-09-08 23:56:54 +0100

    Update download-tiktok.js

--- plugins/download-tiktok.js
+++ plugins/download-tiktok.js
@@ -8,28 +8,26 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     try {
         // Llamada a la API con la URL del video de TikTok usando global.API
-        let res = await fetch(global.API('fgmods', '/api/downloader/tiktok', { url: args[0] }, 'apikey'));
+        let res = await fetch(`https://api.cafirexos.com/api/tiktokv1?url=${args[0]}`)
         let data = await res.json();
-
         // Verificar si la API devolvió un resultado
-        if (!data.result || !data.result.play) {
-            throw new Error("No se encontró el video o hubo un error al procesar la solicitud.");
+        if (!data.resultado) {
+            throw new Error("No se encontró el video o hubo un error al procesar la solicitud.")
         }
 
         // Crear el mensaje con la información del video
         let message = `
 ┌─⊷ *TIKTOK DESCARGA* 
-▢ *Autor:* ${data.result.author.nickname}
-▢ *Usuario:* ${data.result.author.unique_id}
-▢ *Duración:* ${data.result.duration} segundos
-▢ *Likes:* ${data.result.digg_count}
-▢ *Vistas:* ${data.result.play_count}
-▢ *Descripción:* ${data.result.title}
+▢ *Autor:* ${data.resultado.nickname}
+▢ *Usuario:* ${data.resultado.username}
+▢ *Descripción:* ${data.resultado.title}
 └───────────
 `;
 
         // Enviar el video al chat
-        await conn.sendFile(m.chat, data.result.play, 'tiktok.mp4', message, m);
+for (let i = 0; i < (data.resultado.videoUrl ? 1 : data.resultado.imagesUrl.length); i ++) {
+        await conn.sendFile(m.chat, data.resultado.videoUrl ? data.resultado.videoUrl : data.resultado.imagesUrl[i], 'tiktok.mp4', message, m);
+}
     } catch (error) {
         // En caso de error, responder con un mensaje de fallo
         console.error(error);
