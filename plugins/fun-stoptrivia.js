8ccfddd0e76ec349d2e60970bafbe05d55ba4a88
wavenube <wavenub3@gmail.com>
2024-09-08 15:50:35 +0100

    Update fun-stoptrivia.js

--- plugins/fun-stoptrivia.js
+++ plugins/fun-stoptrivia.js
@@ -1,15 +1,15 @@
-let triviaSessions = {};
+import { getTriviaSessions, removeTriviaSession } from './plugins/_triviaManager.js';
 
 const handler = async (m, { conn, command }) => {
   let chatId = m.chat;
 
   if (command === "stoptrivia") {
-    if (!triviaSessions[chatId]) {
+    if (!getTriviaSessions()[chatId]) {
       return conn.sendMessage(m.chat, { text: "No hay ninguna trivia activa en este chat." });
     }
 
     // Finalizar trivia
-    delete triviaSessions[chatId];
+    removeTriviaSession(chatId);
     conn.sendMessage(m.chat, { text: "✅ Trivia finalizada. No se puede responder más hasta que se inicie una nueva trivia." });
   }
 };
