eccd584c7d4980e6ab8674877b1c173722c88edd
wavenube <wavenub3@gmail.com>
2024-09-11 17:43:41 +0100

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -1,11 +1,9 @@
-import { createHash } from 'crypto';
-import PhoneNumber from 'awesome-phonenumber';
 import axios from 'axios';
 
-const handler = async (m, { conn, usedPrefix, participants, isPrems }) => {
+const handler = async (m, { conn, usedPrefix }) => {
   // Identificar al usuario mencionado o al emisor del mensaje
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  
+
   // Verificar que el usuario esté registrado en la base de datos
   if (!(who in global.db.data.users)) {
     throw 'El usuario no está registrado.';
@@ -13,20 +11,21 @@ const handler = async (m, { conn, usedPrefix, participants, isPrems }) => {
 
   try {
     // Obtener la imagen de perfil del usuario
-    let pp = await conn.profilePictureUrl(who);
-    
-    // En caso de error al obtener la foto de perfil, usar una imagen por defecto
-    if (!pp) {
-      pp = 'https://telegra.ph/file/66c5ede2293ccf9e53efa.jpg'; // Imagen por defecto
+    let userProfilePicture;
+    try {
+      userProfilePicture = await conn.profilePictureUrl(who, 'image');
+    } catch (e) {
+      // Si no tiene foto de perfil, usa una imagen por defecto
+      userProfilePicture = 'https://telegra.ph/file/66c5ede2293ccf9e53efa.jpg'; // Imagen por defecto si no tiene foto
     }
 
     // Obtener datos del usuario desde la base de datos
     const { name, limit, registered, age, premiumTime } = global.db.data.users[who];
     const username = conn.getName(who);
     const sn = createHash('md5').update(who).digest('hex');
-    
+
     // Construir la URL de la API
-    const apiUrl = `https://deliriusapi-official.vercel.app/canvas/balcard?url=${encodeURIComponent(pp)}&background=https://telegra.ph/file/66c5ede2293ccf9e53efa.jpg&username=${encodeURIComponent(username)}&discriminator=${sn}&money=${limit}&xp=1000&level=10`;
+    const apiUrl = `https://deliriusapi-official.vercel.app/canvas/balcard?url=${encodeURIComponent(userProfilePicture)}&background=https://telegra.ph/file/66c5ede2293ccf9e53efa.jpg&username=${encodeURIComponent(username)}&discriminator=${sn}&money=${limit}&xp=1000&level=10`;
 
     // Obtener la imagen de la API
     let response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
