144af931f9361f1b02b8fbac9fa5f54380ce3fec
wavenube <wavenub3@gmail.com>
2024-09-13 17:48:50 +0100

    Delete plugins/_pausa.js

--- plugins/_pausa.js
+++
@@ -1,9 +0,0 @@
-const pauseHandler = async (m, { conn, isOwner }) => {
-  if (!isOwner) return m.reply('❌ Solo los administradores pueden pausar el bot.');
-  
-  isPaused = true;
-  m.reply('⏸️ El bot ha sido pausado. Usa ".start" para reactivarlo.');
-};
-
-handler.command = /^pausa$/i;
-export default pauseHandler;
