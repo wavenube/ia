a93843812cb668d99ed42f7eab94104256e5ad13
wavenube <wavenub3@gmail.com>
2024-09-13 17:48:58 +0100

    Delete plugins/_start.js

--- plugins/_start.js
+++
@@ -1,9 +0,0 @@
-const startHandler = async (m, { conn, isOwner }) => {
-  if (!isOwner) return m.reply('❌ Solo los administradores pueden reactivar el bot.');
-
-  isPaused = false;
-  m.reply('▶️ El bot ha sido reactivado y ahora acepta comandos nuevamente.');
-};
-
-handler.command = /^start$/i;
-export default startHandler;
