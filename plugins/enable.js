8daf0ce6d23180e8db6782379236b40eb8da93c9
wavenube <wavenub3@gmail.com>
2024-09-28 15:12:43 +0100

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -53,6 +53,16 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       }
       chat.antidelete = isEnable;
       break;
+
+       case 'anticrash':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+          throw false;
+        }
+      }
+      chat.anticrash = isEnable;
+      break;
       
     case 'antilink':
       if (m.isGroup) {
@@ -118,6 +128,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
 ▢ antilink
 ▢ detect 
 ▢ modohorny
+▢ anticrash
 └─────────────
 ┌─⊷ *OWNER*
 ▢ public/private
